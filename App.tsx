d0fa7939d1a9043b49090874e241aabf7792dd53
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-05 07:56:18 +0200

    Refactor the setup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable react/style-prop-object */
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -13,12 +19,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
-);
-
-export default App;

55c2dba97ee14bc1ea50a995fc170938189d2315
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-06 22:22:30 +0200

    Authentication screen and redux initialization

---
+++ src/components/ButtonWithTittle/ButtonWithTitle.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react-native/no-color-literals */
+import React, { } from 'react'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+
+interface ButtonProps{ 
+    onTap: Function;
+    width: number;
+    height: number;
+    title: string,
+    isNoBg?: boolean
+ }
+
+const ButtonWithTitle: React.FC<ButtonProps> = ({ onTap, width, height, title, isNoBg = false }) => {
+
+    if(isNoBg){
+        return (
+            <TouchableOpacity style={[styles.btn, { width, height, backgroundColor:'transparent' } ]} 
+                onPress={() =>  onTap()}
+            >
+                <Text style={{ fontSize: 16, color: '#3980D9'}}>{title}</Text>
+            </TouchableOpacity>
+        )
+    }else{
+        
+        return (
+            <TouchableOpacity style={[styles.btn, { width, height } ]} 
+                onPress={() =>  onTap()}
+            >
+                <Text style={{ fontSize: 16, color: '#FFF'}}>{title}</Text>
+            </TouchableOpacity>
+        )
+
+
+    }
+
+}
+
+
+const styles = StyleSheet.create({
+    btn: { 
+        flex: 1,
+        display: 'flex',  
+        maxHeight: 50,
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: '#f15b5d',
+        borderRadius:30,
+        alignSelf: 'center',
+        marginTop: 20,
+    },
+    
+})
+
+ export { ButtonWithTitle }

bb784b2bd0704ab0feb098ede3fe2fde76c183fd
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-08 03:42:12 +0200

    Minor update

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,8 +2,42 @@ interface Identity {
     name: string;
     firstName: string;
     lastName: string;
-}
+};
+
+interface Address {
+    completeAddress: string
+};
+
+interface Course {
+    label: string;
+    ponderation: number;
+    hoursPerWeek: number;
+    hasExamSession: boolean;
+    hasSecondSession: boolean;
+};
+
+interface Qualification {
+    degree: "A0" | "A1" | "A2" | "D6" | "L2" | "G3" | "MD";
+    longetivity: number
+};
 
 interface Teacher {
-    identity: Identity
+    identity: Identity;
+    address: Address,
+    courses: [Course]
+    qualification: Qualification
+};
+
+interface Class {
+    isCO: boolean;
+    isCT: boolean;
+    courses: [Course];
+    label: string;
+    students: [Student]
+};
+
+interface Student {
+    identity: Identity;
+    guardians: [Guardian] // Tutaire in french
+    class: Class
 }

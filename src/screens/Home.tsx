4f0f0eb015cd709d96b864c8a924b198c54e5e51
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-15 14:50:00 +0200

    Update the home header compoenent

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,14 +3,19 @@ import {
     ScrollView,
     View,
     StyleSheet,
+    Text,
     TouchableOpacity
 } from "react-native";
+import { Avatar } from 'react-native-paper';
 
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 
 import { CardOptions, HeaderHome } from "../components/";
 import { HomeOption } from "../types";
 import img from "../assets/icon.jpeg";
+import avatar from "../assets/teacher.png";
+import { bgTextInputColor, whiteColor } from "../core";
 
 // interface Props {
 //     options: [HomeOption]
@@ -26,9 +31,18 @@ const Home:React.FC = () => {
         <ScrollView>
             <View style={styles.header}>
                 <View style={styles.headerSubContainer}>
-                    {/* <TouchableOpacity> */}
-                        <Icon name="menu" size={30} />
-                    {/* </TouchableOpacity> */}
+                    <TouchableOpacity style={styles.humburger}>
+                        <Icon name="menu" size={30} color="#000" />
+                    </TouchableOpacity>
+                    <View style={styles.rightHeader}>
+                        <TouchableOpacity style={{marginRight: 20}}>
+                            <Text style={styles.badge}>10</Text>
+                            <MaterialIcon name="notifications-none" color={bgTextInputColor} size={30} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Avatar.Image size={40} source={avatar} />
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
             <HeaderHome school={{label: "ITIG Don Bosco", slogan: "Duc In Altum"}} />
@@ -59,7 +73,29 @@ const styles = StyleSheet.create({
     },
     headerSubContainer: {
         flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        justifyContent: "space-between"
+    },
+    rightHeader: {
         flexDirection: "row"
+    },
+    humburger: {
+        padding: 5,
+        backgroundColor: bgTextInputColor,
+        borderRadius: 20
+    },
+    badge: {
+        position: "absolute",
+        right: 0, 
+        top: 0,
+        fontSize: 10,
+        backgroundColor: "red",
+        padding: 2,
+        borderRadius: 10,
+        color: whiteColor,
+        zIndex: 1
     }
 })
 

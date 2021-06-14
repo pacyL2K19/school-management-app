953ebf4a56abc6bfb85fe6328103fb3cc1fbd5d0
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-06-14 10:48:32 +0100

    Minor update

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/navigation/';
-import { store } from './src/redux'
-import { Provider } from 'react-redux'
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/navigation/";
+import { store } from "./src/redux";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
--- src/__tests__/LoginScreen.test.tsx
+++ src/__tests__/LoginScreen.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+// import { render, fireEvent, waitForElement } from "@testing-library/react";
 
 // import Login, { Props } from "../screens/Login";
 

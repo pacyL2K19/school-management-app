7e3ef36abdb4f421efd60c68d53137177f097696
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-06-28 21:44:50 +0100

    Minor update

--- src/screens/Remarks.tsx
+++ src/screens/Remarks.tsx
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import ModalDropdown from "react-native-modal-dropdown";
+import { getRemarkStore } from "../ApiClient";
+import { MyGlobalContext } from "../context";
 
 const Remark: React.FC = () => {
   const [details, setDetails] = useState(null);
-  const [remarks, setRemarks] = useState(null);
+  const [remarks, setRemarks] = useState([]);
   const [comment, setComment] = useState("");
   const [selectedStudent, setSelectedStudent] = useState(null);
   const [students, setStudents] = useState(null);
+  const { accountInfo, setAccountInfo } = useContext(MyGlobalContext);
+
+  useEffect(() => {
+    getRemarkStore(accountInfo?.teachearId, accountInfo?.token)
+      .then((res) => {
+        if (res.success) {
+          const optionsRmk = [];
+          res.remarks.forEach((rmk) => {
+            optionsRmk.push(rmk.Category);
+          });
+          setRemarks(optionsRmk);
+        }
+      })
+      .catch();
+  }, []);
   return (
     <View style={styles.header}>
       <Text style={{ marginVertical: 10 }}>Annee Academique</Text>
       <ModalDropdown
         options={["2020-2021", "2019-2020"]}
+        defaultValue="2020-2021"
         style={{
           backgroundColor: "white",
           padding: 10,
@@ -30,12 +48,13 @@ const Remark: React.FC = () => {
       />
       <Text style={{ marginVertical: 10 }}>Type de faute</Text>
       <ModalDropdown
-        options={["2020-2021", "2019-2020"]}
+        options={remarks}
         style={{
           backgroundColor: "white",
           padding: 10,
           borderRadius: 5,
         }}
+        defaultValue="Veuillez selectionner la faute"
         textStyle={{
           fontSize: 14,
         }}
@@ -45,6 +64,7 @@ const Remark: React.FC = () => {
         dropdownTextStyle={{
           fontSize: 12,
         }}
+        
       />
     </View>
   );
